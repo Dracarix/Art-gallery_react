a3513b047e98cf0fe7a5d0a18d9c3e085f2f9d37
Dracarix <lebedkov200002@gmail.com>
2023-12-19 20:53:28 +0300

    update images

--- src/components/UI/ImgBox/ImgBox.module.scss.d.ts
+++ src/components/UI/ImgBox/ImgBox.module.scss.d.ts
@@ -2,5 +2,6 @@ declare const styles: {
   readonly ImgBoxed: string;
   readonly cartInfo: string;
   readonly ArtSection: string;
+  readonly loadingBar: string;
 };
 export = styles;
--- src/components/UI/ImgBox/ImgBox.tsx
+++ src/components/UI/ImgBox/ImgBox.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unsafe-optional-chaining */
+import React, { useEffect, useState } from 'react';
 import cl from './ImgBox.module.scss';
 
 interface SelectImg {
@@ -11,31 +13,88 @@ interface SelectImg {
     locationID: string;
   }>;
 }
+
 function ImgBox({ images }: SelectImg) {
-  const baseUrl = 'https://test-front.framework.team/';
+  const baseUrl = 'https://test-front.framework.team';
+  const [loadedImages, setLoadedImages] = useState<number[]>([]);
+  const [loadingAttempts, setLoadingAttempts] = useState<{ [key: number]: number }>({});
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleImageLoad = (value: number) => {
+    setLoadedImages((prevLoadedImages) => [...prevLoadedImages, value]);
+  };
+
+  const handleImageError = (value: number) => {
+    setLoadingAttempts((prevAttempts) => ({
+      ...prevAttempts,
+      [value]: (prevAttempts[value] || 0) + 1,
+    }));
+  };
+
+  useEffect(() => {
+    const loadImages = async () => {
+      setIsLoading(true);
+
+      await Promise.all(
+        Object.keys(loadingAttempts).map(async (value) => {
+          const intValue = Number(value);
+          if (loadingAttempts[intValue] < 3 && !loadedImages.includes(intValue)) {
+            const imgElement = document.querySelector(`img[data-value="${intValue}"]`) as HTMLImageElement | null;
+
+            if (imgElement) {
+              try {
+                await fetch(baseUrl + images.find((img) => img.value === intValue)?.imageUrl);
+                imgElement.src = baseUrl + images.find((img) => img.value === intValue)?.imageUrl;
+                handleImageLoad(intValue);
+              } catch (error) {
+                // eslint-disable-next-line no-alert
+              }
+            }
+          }
+        }),
+      );
+
+      setIsLoading(false);
+    };
+
+    loadImages();
+  }, [loadingAttempts, baseUrl, images, loadedImages]);
+
   return (
     <div className={cl.ArtSection}>
       {images.map((imag) => (
         <div className={cl.ImgBoxed} key={imag.value}>
-          <img src={baseUrl + imag.imageUrl} alt={imag.name} />
-          <div className={cl.cartInfo}>
-            <h3>{imag.name}</h3>
-            <span>
-              Author:
-              {' '}
-              <p>{imag.authors}</p>
-            </span>
-            <span>
-              Created:
-              {' '}
-              <p>{imag.created}</p>
-            </span>
-            <span>
-              Location:
-              {' '}
-              <p>{imag.locationID}</p>
-            </span>
-          </div>
+          <img
+            src={baseUrl + imag.imageUrl}
+            alt={imag.name}
+            onLoad={() => handleImageLoad(imag.value)}
+            onError={() => handleImageError(imag.value)}
+            data-value={imag.value}
+          />
+          {loadedImages.includes(imag.value) ? (
+            <div className={cl.cartInfo}>
+              <h3>{imag.name}</h3>
+              <span>
+                Author:
+                {' '}
+                <p>{imag.authors}</p>
+              </span>
+              <span>
+                Created:
+                {' '}
+                <p>{imag.created}</p>
+              </span>
+              <span>
+                Location:
+                {' '}
+                <p>{imag.locationID}</p>
+              </span>
+            </div>
+          ) : (
+            <div className={cl.loadingBar} />
+          )}
+
         </div>
       ))}
     </div>
